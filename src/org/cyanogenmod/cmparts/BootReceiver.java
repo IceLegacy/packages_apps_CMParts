LineageOS/android_packages_apps_CMParts

CMParts: Add touchscreen gesture settings and keyhandler
With the new TouchscreenGesture CMHW API, devices can advertise
support for different sorts of touchscreen gestures.

Add settings in CMParts to allow users to configure each and
every one of the gesture to their liking.

Change-Id: I7c0f80cbae52ee85b9fdf12bdcebe99a78febd6a

src/org/cyanogenmod/cmparts/BootReceiver.java
@@ -23,6 +23,7 @@ import android.content.SharedPreferences;
 import android.support.v7.preference.PreferenceManager;
 
 import org.cyanogenmod.cmparts.contributors.ContributorsCloudFragment;
+import org.cyanogenmod.cmparts.gestures.GesturesSettings;
 import org.cyanogenmod.cmparts.input.ButtonSettings;
 
 public class BootReceiver extends BroadcastReceiver {
@@ -37,6 +38,8 @@ public class BootReceiver extends BroadcastReceiver {
             ButtonSettings.restoreKeyDisabler(ctx);
             setRestoredTunable(ctx);
         }
+
+        GesturesSettings.restoreTouchscreenGestureStates(ctx);
 
         // Extract the contributors database
         ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);

IceLegacy/packages_apps_CMParts

src/org/cyanogenmod/cmparts/BootReceiver.java
@@ -23,6 +23,7 @@ import android.content.SharedPreferences;
 import android.support.v7.preference.PreferenceManager;
 
 import org.cyanogenmod.cmparts.contributors.ContributorsCloudFragment;
+import org.cyanogenmod.cmparts.gestures.TouchscreenGestureSettings;
 import org.cyanogenmod.cmparts.input.ButtonSettings;
 
 public class BootReceiver extends BroadcastReceiver {
@@ -37,6 +38,8 @@ public class BootReceiver extends BroadcastReceiver {
             ButtonSettings.restoreKeyDisabler(ctx);
             setRestoredTunable(ctx);
         }
+
+        TouchscreenGestureSettings.restoreTouchscreenGestureStates(ctx);
 
         // Extract the contributors database
         ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);